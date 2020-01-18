f10da444d7d76ebb58316aa252fe18590a7e24bf
Jacob Allen West <wjacoba@tajmahal.cs.byu.edu>
2020-01-17 17:26:25 -0700

    Everything should be working now

--- myxxd.c
+++ myxxd.c
@@ -102,12 +102,80 @@ void printDataAsChars(unsigned char *data, size_t size) {
  * See myxxd.md for details.
  *
  * data: an array of no more than 6 bytes
- * size: the size of the array
+ * size: number of characters in the array
  **/
 
 void printDataAsBinary(unsigned char *data, size_t size) {
- // We will get one char, get its int value, and convert that to be a binary number 
- 
+
+	int bytesLacking = (6 - size);
+
+	for(int i = 0; i < size; i++) {
+		int charAsIntValue = data[i];
+		int byte[8] = { 0 };
+	
+	
+		int storedBitNums = 0;
+		int tempCharAsIntValue = data[i];
+		while(tempCharAsIntValue >= 2) { // 2 Because binary is base 2
+			tempCharAsIntValue /= 2;
+			storedBitNums++;
+			if (tempCharAsIntValue == 1)	 {
+				storedBitNums++; // We do this because there is technically
+						 // one more round to do, but our comparison
+						 // algorithm is stinky so it won't really
+						 // let us do it. This is compensation.
+			}
+		}
+
+
+		// We now know how many non zero spaces we need to fill for our array
+		
+		// Time to find what the bits are
+		
+		int emptyBitNums = 8 - storedBitNums;
+
+		// We are just filling the array with empty bit nums for now
+		for (int j = 0; j < emptyBitNums; j++) {
+			byte[j] = 0;
+		}
+
+
+		// Start filling byte[] from right to leftwith our calculations.
+		// We have filled leading zeros with the size of emptyBitNums.
+		// We will fill with the index after that.
+		
+		for (int j = 7; j > (emptyBitNums - 1); j--) {
+			byte[j] = charAsIntValue % 2;
+			charAsIntValue /= 2;
+		}
+
+		// The array is now filled correctly. Now we just need to print bytes[]
+		// from left to right in order.
+
+		for (int j = 0; j < 8; j++) {
+			printf("%i", byte[j]);		
+		}
+
+		// The entire byte has been printed. Now print a whitespace to match format
+		// between bytes. We have to have this special work around my for loop
+		// beneath this one for some reason doesn't work with my overall logic. Lol.
+		
+		if((bytesLacking > 0) && (i != (size - 1))) {
+			printf(" ");	
+		} else if ((bytesLacking == 0) && (i != (size - 1))){
+			printf(" ");
+		}
+	}
+
+	// Now we will handle the whitespace. Each byte is 8 bits plus one space. 
+	// So that is 9 characters total. We will printf(" "); 8 times for each 
+	// byte that we are short of 6
+	
+	for(int i = 0; i < bytesLacking; i++) {
+		for(int j = 0; j < 9; j++) {
+			printf(" ");
+		}	
+	}
 }
 
 void readAndPrintInputAsHex(FILE *input) {
@@ -145,7 +213,7 @@ void readAndPrintInputAsBits(FILE *input) {
     printf("  ");
     printDataAsChars(data, numBytesRead);
     printf("\n");
-    numBytesRead = fread(data, 1, 6 input);
+    numBytesRead = fread(data, 1, 6, input);
   }
 }
 
